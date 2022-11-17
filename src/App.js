5cd2c3858736f0ea60ad047c454759289bcb22d0
roman wiens <wiensroman95@gmail.com>
2022-11-18 00:01:49 +0600

    pagination refactoring

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import PostService from './API/PostService';
 import Loader from './components/UI/Loader/Loader';
 import {useFetching} from './hooks/useFetching';
 import {getPagesArray,getPageCount} from './utils/pages';
+import Pagination from './components/UI/pagination/Pagination';
 
 //
 function App() {
@@ -28,7 +29,7 @@ function App() {
   const sortedAndSearchedPosts = usePosts(posts,filter.sort,filter.query);
   const [limit,setLimit] = useState(10);
   const [page,setPage] = useState(1);
-  let pagesArray = getPagesArray(totalPages);
+  
 
   
   const [fetchPosts,isPostsLoading,postError] = useFetching(async (limit,page) => {
@@ -81,18 +82,11 @@ function App() {
       ? <div style={{display: 'flex',justifyContent: 'center'}}><Loader /></div>
        : <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS"/>
         }
-        <div className="page__wrapper">
-          {pagesArray.map(p => 
-          <span 
-            onClick={() => changePage(p)}
-            key={p}
-            className={page === p ? 'page page__current' : 'page'}  
-          >
-            {p}
-          </span>
-          )}
-        </div>
-        
+        <Pagination 
+        page={page}
+        changePage={changePage}
+        totalPages={totalPages}
+        />
   </div>
   );
 }
